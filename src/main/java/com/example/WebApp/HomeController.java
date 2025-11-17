f90cbe6238f8ca64a6164b2bd09d469eddecbc48
Lana <alana.rayanne@gmail.com>
2025-11-17 16:18:32 -0300

    1o commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World!";
+	}
+	
+	@RequestMapping("/lana")
+	public @ResponseBody String greetingLana() {
+		return "Hello, Lana!";
+	}
+	
+
+}
